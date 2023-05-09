fa03ef0d6b290c0108f6e6bc5158eb31cfd4fbca
annkarpe <karpenkoannaig@gmail.com>
2023-05-09 23:27:58 +0200

    Marker clustering added

--- dash/app.py
+++ dash/app.py
@@ -1,5 +1,6 @@
 from dash import Dash, html
 import dash_leaflet as dl
+import dash_leaflet.express as dlx
 from dash import Dash, html
 import requests
 import json
@@ -10,16 +11,13 @@ data = json.loads(response.text)
 app = Dash(__name__)
 app.layout = html.Div([
     dl.Map([
-        dl.LayersControl(
-            [dl.BaseLayer(dl.TileLayer(), checked=True)] + [dl.Overlay(
-                dl.Marker(position=(d['latitude'], d['longitude'])), checked=True)
-                  for d in data]
-        )
-        
-    ], id="map", zoom=1, style={'width': '100%', 
+        dl.TileLayer(),
+        dl.GeoJSON(data=dlx.dicts_to_geojson([
+            {'lat': d['latitude'], 'lon': d['longitude']} for d in data]), 
+                cluster=True)],
+        id="map", zoom=1, style={'width': '100%', 
         'height': '50vh', 'margin': "auto", "display": "block"})
 ])
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=8050)
-
